1d2f8c3cc270a85ad7a3d39bce3d6ae5ee0fb2d6
Zagaz <andre_ranulfo@hotmail.com>
2023-10-13 14:13:02 -0300

    It's alive

---
+++ app/Http/Controllers/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+// db class
+use Illuminate\Support\Facades\DB;
+
+
+class Main extends Controller
+{
+    function db()
+    {
+        $users = DB::select('select * from users');
+        foreach ($users as $user) {
+            echo "$user->id) $user->name. ";
+            echo "<br>";
+        }
+    }
+}
